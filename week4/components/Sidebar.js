e67b88380e11a918c8e26f232c579ec47a5dac27
kichacon <kiarachacon8@yahoo.com>
2020-09-23 01:53:34 -0400

    Debugging Assignment 2

--- week4/components/Sidebar.js
+++ week4/components/Sidebar.js
@@ -9,9 +9,17 @@ class Sidebar extends Component {
   render () {
    
     let responseHTML = '';
+    
 
     this.props.menu.forEach( (item) => {
       // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
+      responseHTML += '<li>' + Albert Eggstein + '</li>';
+      responseHTML += '<li>' + Attila the Hen + '</li>';
+      responseHTML += '<li>' + Attila the Hen + '</li>';
+      responseHTML += '<li>' + Dixie Chick + '</li>';
+      responseHTML += '<li>' + Gregory Peck + '</li>';
+      responseHTML += '<li>' + Mary Poppins + '</li>';
+      responseHTML += '<ul>' + responseHTML + '</ul>';
     });
 
     return responseHTML;

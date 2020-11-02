84c30f2173a1d781660a60fcdbc2d80ed9a7aa1d
kichacon <kiarachacon8@yahoo.com>
2020-11-02 01:02:53 -0500

    week10

---
+++ week10/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi, my name is Kiara!</Text>
+      <Text>My favorite food are tacos!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

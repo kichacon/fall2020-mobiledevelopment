10ecf5e325233290ba1606a75878c5c0753cfca5
kichacon <kiarachacon8@yahoo.com>
2020-12-10 23:58:05 -0500

    portfolio2

--- week13stack/App.js
+++ week13stack/App.js
@@ -1,19 +1,27 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function Home(props) {
- 
+class Home extends Component {
+  state = {
+    buttonEnabled:true,
+  }
+  render(){
+ const buttonColor = this.state.buttonEnabled ? 'red' : 'blue'
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.homeText}>Hello, Welcome User!</Text><br></br>
+     <View >
+           <Text style={styles.titleText}>Sign In</Text>
+           <TextInput style={styles.formtext} placeholder="username or e-mail"/> 
+           <TextInput style={styles.formtext} placeholder="password"/> 
+           <TouchableOpacity style={[styles.stylebutton, {backgroundColor:buttonColor}]}><Text style={styles.buttonText}>Sign In</Text></TouchableOpacity>
+      
       <Button title="Start Exercises" onPress={() => props.navigation.navigate('exerciseOne')} />
     </View>
-  );
-}
+  
+}}
 
 function exerciseOne(props) {
   var  [counter, setCounter] = useState(0);
@@ -24,7 +32,7 @@ function exerciseOne(props) {
       <Button style={styles.buttonStyle} title="Increase Reps" onPress={() => {setCounter(counter + 1)}}/> <br></br>
       <Button title="Reset" onPress={() => {setCounter(counter = 0)}}/><br></br>
       <Button title="Next Exercise" onPress={() => props.navigation.navigate('exerciseTwo')} />
-    </View>
+    </View> 
   );
 }
 
@@ -36,7 +44,7 @@ function exerciseTwo(props) {
       <Text style={styles.regularText}>Reps Total: {counter}</Text><br></br>
       <Button title="Increase Reps" onPress={() => {setCounter(counter + 1)}}/> <br></br>
       <Button title="Reset" onPress={() => {setCounter(counter = 0)}}/><br></br>
-      <Button title="Back to Home" onPress={() => props.navigation.navigate('Home')} />
+      <Button title="Back to Home" onPress={() => props.navigation.navigate('Home')} /> 
     </View>
   );
 }
@@ -47,21 +55,54 @@ const styles =StyleSheet.create({
     fontSize: 40,
     fontFamily: "sans-serif"
   },
-  titleText: {
-    color: "orange",
-    fontSize: 50,
-    fontWeight: "bold"
-  },
   homeText: {
     color: "orange",
     fontSize: 70,
     fontFamily: "sans-serif",
     fontWeight: "bold"
     },
-  // buttonStyle: {
-   //   backgroundColor: "white",
-   //   color: "blue"
-   // }
+      container: {
+        flex: 1,
+        backgroundColor: "lightblue",
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      titleText: {
+        color: "black",
+        fontWeight: 'bold',
+        fontSize: "40px",
+        paddingBottom: 100,
+      },
+
+    formtext:{
+        color: "white",
+        fontSize: "20px",
+        paddingBottom: 10,
+        marginBottom: 40,
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        borderTopColor: "black",
+        borderTopWidth: 1,
+        borderLeftColor: "black",
+        borderLeftWidth: 1,
+        paddingLeft: 10,
+        borderRightColor: "black",
+        borderRightWidth: 1,
+        justifyContent: 'center',
+        
+    },
+    stylebutton: {
+        backgroundColor: "white",
+        marginTop: 30, 
+        padding: 20,
+        alignItems: 'center'
+      },
+    buttonText: {
+        color: "gray",
+        fontWeight: 'bold',
+        fontSize: "20px",
+    },
+    
 });
 
 const Stack = createStackNavigator();
